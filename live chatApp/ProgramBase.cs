37c9a22c89845c95552a5ba3762374730c8570c7
Armand Nziza <217035019@mycput.ac.za>
2021-08-28 16:14:47 +0200

    C:\Users\SKCAFE14\Source\Repos\live-chatApp\live chatApp\live chatApp.csproj

---
+++ live chatApp/ProgramBase.cs
@@ -0,0 +1,17 @@
+﻿namespace SignalRChat
+{
+    public class ProgramBase
+    {
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<SignalRChat>();
+                });
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+    }
+}
